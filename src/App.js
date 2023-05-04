f80252dadd1d793c3bc9a472b046ad36ce90ca4e
FionnT <fionntolster@gmail.com>
2023-05-04 21:14:18 +0800

    feat: Finish end-to-end testing

--- src/App.js
+++ src/App.js
@@ -29,6 +29,7 @@ function App() {
   const [applePayState, updateApplePayState] = useState({})
 
   const passToParent = (type, state) => {
+    console.log(type, state)
     if (type === 'google') {
       updateGooglePayState(state)
     } else {
